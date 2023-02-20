d06a5fdecf1c8e64d28b5a43fbccb2ebdacd4f0b
brenopinna <brenopinna1008@gmail.com>
2023-02-20 17:26:41 -0300

    add esmodules

--- script.js
+++ script.js
@@ -1,93 +1,12 @@
-const root = document.querySelector(":root")
-const themeSwitcherButton = document.getElementById("theme-switcher")
-const input = document.getElementById("input")
-const keys = document.querySelectorAll("#keys button")
-const resultInput = document.getElementById("result")
-const copyButton = document.getElementById("copy")
+import { themeSwitcher } from "./modules/theme-switcher.js"
+import { copy } from "./modules/copy-functions.js"
+import { clickInteraction, keyboardInteraction } from "./modules/user-interactions.js"
 
-const allowedKeys = [
-  "(",
-  ")",
-  "/",
-  "*",
-  "-",
-  "+",
-  "9",
-  "8",
-  "7",
-  "6",
-  "5",
-  "4",
-  "3",
-  "2",
-  "1",
-  "0",
-  ".",
-  "%",
-  " ",
-]
-
-input.addEventListener("keydown", e => {
-  e.preventDefault()
-  resetCopyButton()
-  if (allowedKeys.includes(e.key)) {
-    e.target.value += e.key
-  } else if (e.key === "Backspace") {
-    e.target.value = e.target.value.slice(0, -1)
-  } else if (e.key === "Enter") {
-    calculate()
-  } else if (e.key === "c") {
-    clear()
-  }
-})
-
-keys.forEach(key => {
-  key.addEventListener("click", e => {
-    resetCopyButton()
-    if (e.target.classList.contains("charKey")) {
-      input.value += e.target.dataset.value
-    } else if (e.target.id === "clear") {
-      clear()
-    } else if (e.target.id === "equal") {
-      calculate()
-    }
-  })
-})
+import { themeSwitcherButton, copyButton } from "./modules/dom-elements.js"
 
 themeSwitcherButton.addEventListener("click", themeSwitcher)
 
+clickInteraction()
+keyboardInteraction()
+
 copyButton.addEventListener("click", copy)
-
-function themeSwitcher() {
-  root.classList.toggle("light")
-  root.classList.toggle("dark")
-}
-
-function calculate() {
-  try {
-    resultInput.style.backgroundColor = "transparent"
-    resultInput.value = eval(input.value)
-  } catch {
-    resultInput.style.backgroundColor =
-      getComputedStyle(root).getPropertyValue("--error-color")
-    resultInput.value = "ERRO!"
-  }
-}
-
-function clear() {
-  input.value = ""
-}
-
-function copy() {
-  navigator.clipboard.writeText(resultInput.value)
-  copyButton.innerText = "Copied!"
-  const primaryColor = getComputedStyle(root).getPropertyValue("--primary-color")
-  copyButton.style.borderColor = primaryColor
-  copyButton.style.color = primaryColor
-}
-
-function resetCopyButton() {
-  copyButton.innerText = "Copy"
-  copyButton.style.borderColor = getComputedStyle(root).getPropertyValue("--border-color")
-  copyButton.style.color = getComputedStyle(root).getPropertyValue("--font-color")
-}
