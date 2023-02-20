2472e3d58c2c567b0072963d6cf24f5f7d4bfe0a
brenopinna <brenopinna1008@gmail.com>
2023-02-20 18:09:15 -0300

    update user interactions module

--- modules/user-interactions.js
+++ modules/user-interactions.js
@@ -2,35 +2,31 @@ import { resetCopyButton } from "./copy-functions.js"
 import { allowedKeys } from "./allowed-keys.js"
 import { calculate, clear } from "./calculator-functions.js"
 
-import { input, copyButton, root, keys, resultInput } from "./dom-elements.js"
+import { input, copyButton, root, resultInput } from "./dom-elements.js"
 
-const keyboardInteraction = () =>
-  input.addEventListener("keydown", e => {
-    e.preventDefault()
-    resetCopyButton(copyButton, root)
-    if (allowedKeys.includes(e.key)) {
-      e.target.value += e.key
-    } else if (e.key === "Backspace") {
-      e.target.value = e.target.value.slice(0, -1)
-    } else if (e.key === "Enter") {
-      calculate(resultInput, input, root)
-    } else if (e.key === "c") {
-      clear(input)
-    }
-  })
+const keyboardInteraction = e => {
+  e.preventDefault()
+  resetCopyButton(copyButton, root)
+  if (allowedKeys.includes(e.key)) {
+    e.target.value += e.key
+  } else if (e.key === "Backspace") {
+    e.target.value = e.target.value.slice(0, -1)
+  } else if (e.key === "Enter") {
+    calculate(resultInput, input, root)
+  } else if (e.key === "c") {
+    clear(input)
+  }
+}
 
-const clickInteraction = () =>
-  keys.forEach(key => {
-    key.addEventListener("click", e => {
-      resetCopyButton(copyButton, root)
-      if (e.target.classList.contains("charKey")) {
-        input.value += e.target.dataset.value
-      } else if (e.target.id === "clear") {
-        clear(input)
-      } else if (e.target.id === "equal") {
-        calculate(resultInput, input, root)
-      }
-    })
-  })
+const clickInteraction = e => {
+  resetCopyButton(copyButton, root)
+  if (e.target.classList.contains("charKey")) {
+    input.value += e.target.dataset.value
+  } else if (e.target.id === "clear") {
+    clear(input)
+  } else if (e.target.id === "equal") {
+    calculate(resultInput, input, root)
+  }
+}
 
 export { keyboardInteraction, clickInteraction }
--- script.js
+++ script.js
@@ -2,11 +2,14 @@ import { themeSwitcher } from "./modules/theme-switcher.js"
 import { copy } from "./modules/copy-functions.js"
 import { clickInteraction, keyboardInteraction } from "./modules/user-interactions.js"
 
-import { themeSwitcherButton, copyButton } from "./modules/dom-elements.js"
+import { themeSwitcherButton, copyButton, keys } from "./modules/dom-elements.js"
 
 themeSwitcherButton.addEventListener("click", themeSwitcher)
 
-clickInteraction()
-keyboardInteraction()
+input.addEventListener("keydown", keyboardInteraction)
+
+keys.forEach(key => {
+  key.addEventListener("click", clickInteraction)
+})
 
 copyButton.addEventListener("click", copy)
