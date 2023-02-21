472d3db7b693cab2e5d6b10c5754f7d240404d1a
brenopinna <brenopinna1008@gmail.com>
2023-02-21 17:20:38 -0300

    update directories

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
-import { themeSwitcher } from "../modules/theme-switcher.js"
-import { copy } from "../modules/copy-functions.js"
-import { clickInteraction, keyboardInteraction } from "../modules/user-interactions.js"
+import { themeSwitcher } from "./modules/theme-switcher.js"
+import { copy } from "./modules/copy-functions.js"
+import { clickInteraction, keyboardInteraction } from "./modules/user-interactions.js"
 import "../styles/index.css"
 
-import { themeSwitcherButton, copyButton, keys, input } from "../modules/dom-elements.js"
+import { themeSwitcherButton, copyButton, keys, input } from "./modules/dom-elements.js"
 
 themeSwitcherButton.addEventListener("click", themeSwitcher)
 
